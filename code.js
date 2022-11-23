94198eb41d8c8da537d2656f29259cefd0316214
ahmedhsn710 <96973515+ahmedhsn710@users.noreply.github.com>
2022-11-23 22:33:33 +0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,18 @@
+let editor = document.querySelector("#editor");
+let run = document.querySelector("#run-button");
+
+
+let a = ace.edit(editor, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/assembly_x86",
+});
+
+
+
+run.addEventListener('click', () => {
+    const code = a.getValue();
+    console.log(code);
+})
+
+  
+
